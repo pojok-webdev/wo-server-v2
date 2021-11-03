ec060f85dcc74951fe1f80e20d96b8fd7243b905
pujie <pw.prayitno@gmail.com>
2021-11-03 15:36:09 +0700

    change port

--- js/configs.js
+++ js/configs.js
@@ -2,7 +2,7 @@ sql = () => {
     return {host:'localhost',database:'teknis',user:'teknis',password:'teknis'}
 }
 appSetting = ()=>{
-    return {port:'20212'}
+    return {port:'20215'}
 }
 module.exports ={
     sql:sql,
