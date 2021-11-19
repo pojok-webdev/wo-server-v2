9a66b5a4b43272dfb0acbe2dd95bcbade28e184e
pujie <pw.prayitno@gmail.com>
2021-11-19 16:51:10 +0700

    add check mandatory

--- js/checkReport.js
+++ js/checkReport.js
@@ -1,14 +1,53 @@
-checkParent = (shouldhave,obj) => {
-    console.log('Should have',shouldhave.parent_fields)
-    console.log('OBJ',obj)
-    obj.forEach(el=>{
-        console.log('El',el)
-        for(let attr in el){
-            console.log('Attr',attr,el[attr])
+checkParentx = (params,mandatories) => {
+    console.log('Req Body',params.parent_fields)
+    console.log('Mandatories',mandatories)
+    mandatories.forEach(mandatory=>{
+        console.log('Each Mandatory',mandatory)
+        for(let mandatory_key in mandatory){
+            console.log('Param by Mandatory',params[mandatory_key])
+            console.log('Key of mandatory',mandatory_key,mandatory[mandatory_key])
+            //check if params have mandatory_key
+            if(!params.hasOwnProperty(mandatory_key)){
+                return {result:false,description:'Should have '+mandatory_key}
+            }else{
+                return {result: true,description:'COmplete'}
+            }
         }
     })
-    return obj
+//    return {result:true, mandatories:mandatories}
 }
+checkParent = (params,mandatories) => {
+    //get the mandatories first
+    console.log('Mandatories',mandatories)
+    let checkman = new Promise((resolve,reject)=>{
+        mandatories.forEach(mandatory=>{
+            for(let mandatory_key in mandatory){
+                console.log('Mandatory key',mandatory_key)
+                //check if mandatory_key in params
+                if(!(mandatory_key in params)){
+                    console.log('It has no mandatory',mandatory_key)
+                    reject ({result:false,description:'Has no',mandatory_key})
+                }else{
+                    console.log('It has mandatory',mandatory_key)
+                    console.log('Parman',params[mandatory_key])
+                }
+            }
+        })
+        resolve({result:true,description:'OK'})
+    })
+    checkman.then(
+        result=>{
+            console.log('Result',result)
+            return result
+        },
+        err=>{
+            console.log('Err',err)
+            return err
+        }
+    )
+    //return mandatories
+}
+
 module.exports = {
     check: (obj,mandatory,allfields) => {
         let table = Object.keys(obj)
