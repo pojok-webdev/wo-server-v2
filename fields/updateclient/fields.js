a61a5b3ebe8035d69a49c4c1795298a22a32dc7d
pujie <pw.prayitno@gmail.com>
2022-01-11 16:17:58 +0700

    add client update help and columns available

--- fields/updateclient/fields.js
+++ fields/updateclient/fields.js
@@ -1,6 +1,6 @@
 var updateClient = {
     mandatories : ['id'],
-    allfields : ['id','name','phone_area','phone','address','city','end_of_contract','business_field','status','alias','user_id','sale_id','siup','npwp','clientcategory','isffr','isoryza','active','prospectdate'],
+    allfields : ['id','name','phone_area','phone','fax_area','fax','address','billaddress','city','end_of_contract','business_field','status','alias','user_id','sale_id','siup','npwp','clientcategory','isffr','isoryza','active','prospectdate','period1','period2'],
     numberfields : []
 }
 module.exports = {
